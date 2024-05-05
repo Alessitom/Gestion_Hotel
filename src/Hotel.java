7498e44bff6200a76213fd84144fb68c312801a7
Alessitom <alexmolerotorres@gmail.com>
2024-05-05 20:52:33 +0200

    Siguiente actualizacion: cambiar disponibilidad a enum

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,17 +1,27 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Hotel {
 
-    private Map<Integer,Habitacion>ListaHotel;
+    private static Map<Integer,Habitacion>ListaHotel;
+    private static final int CONST_NUM_HAB = 100;
 
     public Hotel(Map<Integer, Habitacion> listaHotel) {
-        ListaHotel = new HashMap<>(100);
+        ListaHotel = new HashMap<>(CONST_NUM_HAB);
 
     }
 
-    public static void rellenarHotel(Map ListaHotel){
+    public static void rellenarHotel(){
+        Random random = new Random();
+        for(int i=1;i<=CONST_NUM_HAB;i++){
+           int numeroHab = i;
+            Tipo tipo = Tipo.values()[random.nextInt(Tipo.values().length)];
+            float precio = Float.parseFloat(String.format("%.2f", 50 + Math.random() * 450));
+            boolean disponibilidad = random.nextBoolean();
+            ListaHotel.put(numeroHab,new Habitacion(numeroHab,tipo, disponibilidad, precio));
 
+        }
     }
 
 
